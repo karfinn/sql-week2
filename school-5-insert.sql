2ce1e1979295f88c33f3a8bdbf4cd986ebba3acf
Karina Finn <karina.finn@kellogg.northwestern.edu>
2024-01-16 15:07:15 +0000

    wk3 CRUD

---
+++ school-5-insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO students (first_name, last_name, phone)
+VALUES(
+    "Karina",
+    "Engine",
+    "111-222-333"
+    );
---
+++ school-6-update.sql
@@ -0,0 +1,4 @@
+
+UPDATE students
+SET phone = "202-327-3486"
+WHERE id=1;
